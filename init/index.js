edb4b61637e42f1ea36bcc2cd5b309aaa48517d5
Yash Koparkar <koparkaryash41@gmail.com>
2026-01-06 23:38:55 +0530

    changes done

--- init/index.js
+++ init/index.js
@@ -1,26 +1,22 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../Models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
-
-main()
-.then(() =>{
-    console.log("connected to DB ");
-})
-.catch((err) =>{
-    console.log(err);
-})
+const MONGO_URL = process.env.ATLASDB_URL;
 
 async function main(){
     await mongoose.connect(MONGO_URL);
+    console.log("Connected to Atlas for seeding");
 }
+
+main();
 
 const initDB = async () =>{
     await Listing.deleteMany({});
     initData.data = initData.data.map((obj) => ({ ...obj, owner:"69252a0929e3104f275321e2"}));
     await Listing.insertMany(initData.data);
-    console.log("data was initailed");
+    console.log("Data initialized in Atlas");
 };
 
 initDB();
